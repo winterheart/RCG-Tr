f08dfdcb21653de0519dc48480793b85b7a34d14
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2020-01-24 02:25:26 +0300

    Initial commit

---
+++ lib/rcg_l10n.py
@@ -0,0 +1,195 @@
+# This file is part of River City Girls L10n project
+# Licensed under GPL-3+ License
+# (c) 2020 Azamat H. Hackimov <azamat.hackimov@gmail.com>
+
+from datetime import datetime
+from enum import Enum
+from itertools import zip_longest
+from os import makedirs
+from os.path import join, exists
+from polib import POEntry, POFile, pofile, unescape
+from simplejson import OrderedDict
+import simplejson as json
+
+
+class RcgJsonKeys(Enum):
+    # Some constants from JSON dictionary
+    NON_DIALOG_KEYS = "Non_Dialog_Keys"
+    USEABLES_KEYS = "Useables_Keys"
+    MOVE_KEYS = "Move_Keys"
+    QUEST_KEYS = "Quest_Keys"
+    QUESTITEM_KEYS = "QuestItem_Keys"
+    EQUIP_KEYS = "Equip_Keys"
+    STORE_KEYS = "Store_Keys"
+    DIALOG_KEYS = "Dialog_Keys"
+    TUTORIAL_KEYS = "Tutorial_Keys"
+    METADATA_KEYS = "MetaData_Keys"
+
+
+class RcgLanguages(Enum):
+    LANG_KEY = {"key": "Key", "iso_code": ""}
+    LANG_ENGLISH = {"key": "English", "iso_code": "en"}
+    LANG_FRENCH = {"key": "French", "iso_code": "fr"}
+    LANG_GERMAN = {"key": "German", "iso_code": "de"}
+    LANG_ITALIAN = {"key": "Italian", "iso_code": "it"}
+    LANG_SPANISH = {"key": "Spanish", "iso_code": "es"}
+    LANG_JAPANESE = {"key": "Japanese", "iso_code": "ja"}
+    LANG_KOREAN = {"key": "Korean", "iso_code": "ko"}
+    LANG_CHINESESIMPLIFIED = {"key": "ChineseSimplified", "iso_code": "zh-cn"}
+    LANG_CHINESETRADITIONAL = {"key": "ChineseTraditional", "iso_code": "zh-tw"}
+    LANG_RUSSIAN = {"key": "Russian", "iso_code": "ru"}
+
+
+class RcgTranslation:
+    def __init__(self):
+        """Init class for handling RCG_LocalizationData.json"""
+        self.json_content = OrderedDict([])
+        for item in RcgJsonKeys:
+            self.json_content.update(
+                {item.value: []}
+            )
+
+    def load_json(self, json_path):
+        """
+        Load from json file
+        :param json_path: path to JSON file
+        :return:
+        """
+
+        with open(json_path, "r", encoding="utf-8-sig") as read_file:
+            self.json_content = json.load(read_file)
+        return
+
+    def save_json(self, json_path):
+        """
+        Save class as complete RCG_LocalizationData.json file
+        :param json_path: path to JSON file
+        :return:
+        """
+        with open(json_path, "w", encoding="utf-8-sig", newline="\r\n") as write_file:
+            json.dump(self.json_content, write_file, ensure_ascii=False, indent=2)
+        return
+
+    def save_pot(self, path, json_root_key):
+        """
+        Save Gettext POT file from JSON root key into path
+        :param path: Directory path save to
+        :param json_root_key: JSON key from RcgJsonKeys class
+        :return:
+        """
+        pot = POFile()
+        pot.metadata = {
+            'Project-Id-Version': '1.0',
+            'Report-Msgid-Bugs-To': 'you@example.com',
+            'POT-Creation-Date': datetime.now().isoformat(" "),
+            'PO-Revision-Date': datetime.now().isoformat(" "),
+            'Last-Translator': 'you <you@example.com>',
+            'Language-Team': '',
+            'MIME-Version': '1.0',
+            'Content-Type': 'text/plain; charset=utf-8',
+            'Content-Transfer-Encoding': '8bit',
+        }
+
+        for entry in self.json_content[json_root_key.value]:
+            if entry[RcgLanguages.LANG_ENGLISH.value["key"]] != "":
+                po_entry = POEntry(
+                    msgctxt=entry[RcgLanguages.LANG_KEY.value["key"]],
+                    msgid=entry[RcgLanguages.LANG_ENGLISH.value["key"]],
+                )
+                pot.append(po_entry)
+
+        if not exists(path):
+            makedirs(path)
+        pot.save(join(path, json_root_key.value + ".pot"))
+
+        return
+
+    def load_po(self, path, json_root_key, lang):
+        """
+        Load content of Gettext PO file (from "path/lang/json_root_key.po") into already loaded JSON content
+        :param path: Root directory
+        :param json_root_key: JSON key from RcgJsonKeys class
+        :param lang: Language. Should be in RcgLanguages class
+        :return:
+        """
+
+        if len(self.json_content[json_root_key.value]) == 0:
+            print("WARNING: {} JSON entry is empty! Forgot ot load_json()?"
+                  .format(json_root_key.value))
+            return
+
+        temp_json = OrderedDict([])
+        temp_json.update({json_root_key.value: []})
+
+        language = next(name for name in RcgLanguages if name.value["iso_code"] == lang)
+
+        po = pofile(join(path, lang, json_root_key.value + ".po"))
+        valid_entries = [e for e in po if not e.obsolete and e.translated() and "fuzzy" not in e.flags]
+
+        for entry in valid_entries:
+            temp_json[json_root_key.value].append({
+                RcgLanguages.LANG_KEY.value["key"]: entry.msgctxt,
+                language.value["key"]: entry.msgstr
+            })
+
+        # I wish if I know what is going on here...
+        self.json_content[json_root_key.value] = [{**u, **v} for u, v in zip_longest(
+            self.json_content[json_root_key.value], temp_json[json_root_key.value], fillvalue={})]
+
+        return
+
+    def save_po(self, path, json_root_key, lang, fuzzy=True, overwrite=False):
+        """
+        Save Gettext PO file into directory structure as "path/lang/json_root_key.po"
+
+        WARNING: Do not use method twice, PO files will not be updated, they will be recreated!
+        Any changes between calls will be lost! Function has safeguard parameter overwrite which
+        will not allow overwrite file if it already exists.
+        :param path: Root directory where place to files
+        :param json_root_key: JSON key from RcgJsonKeys class
+        :param lang: Language to translate. Should be in RcgLanguages class
+        :param fuzzy: Make translated entries fuzzy (default is True)
+        :param overwrite: Force overwrite file even if it exists
+        :return:
+        """
+        po = POFile()
+        po.metadata = {
+            'Project-Id-Version': '1.0',
+            'Report-Msgid-Bugs-To': 'you@example.com',
+            'POT-Creation-Date': datetime.now().isoformat(" "),
+            'PO-Revision-Date': datetime.now().isoformat(" "),
+            'Last-Translator': 'you <you@example.com>',
+            'Language-Team': '',
+            'MIME-Version': '1.0',
+            'Content-Type': 'text/plain; charset=utf-8',
+            'Content-Transfer-Encoding': '8bit',
+        }
+
+        language = next(name for name in RcgLanguages if name.value["iso_code"] == lang)
+
+        for entry in self.json_content[json_root_key.value]:
+            if entry[RcgLanguages.LANG_ENGLISH.value["key"]] != "":
+                po_entry = POEntry(
+                    msgctxt=entry[RcgLanguages.LANG_KEY.value["key"]],
+                    msgid=entry[RcgLanguages.LANG_ENGLISH.value["key"]],
+                    msgstr=entry[language.value["key"]],
+                )
+                if fuzzy and "fuzzy" not in po_entry.flags:
+                    po_entry.flags.append("fuzzy")
+                po.append(po_entry)
+
+        save_path = join(path, lang)
+        save_file = join(save_path, json_root_key.value + ".po")
+        if not exists(save_path):
+            makedirs(save_path)
+        if exists(save_file):
+            if overwrite:
+                print("WARNING! File \"{}\" already exists, but overwrite is enabled! Overwriting file!"
+                      .format(save_file))
+                po.save(join(path, lang, json_root_key.value + ".po"))
+            else:
+                print("WARNING! File \"{}\" already exists! Will not saving!"
+                      .format(save_file))
+
+        return
+
---
+++ rcg_translate.py
@@ -0,0 +1,20 @@
+# This file is part of River City Girls L10n project
+# Licensed under GPL-3+ License
+# (c) 2020 Azamat H. Hackimov <azamat.hackimov@gmail.com>
+
+import json
+from lib.rcg_l10n import RcgJsonKeys, RcgLanguages, RcgTranslation
+
+rcg_file = RcgTranslation()
+rcg_file.load_json("data/RCG_LocalizationData.json")
+rcg_file.load_po("translation", RcgJsonKeys.NON_DIALOG_KEYS, RcgLanguages.LANG_RUSSIAN.value["iso_code"])
+rcg_file.save_json("data/translation.json")
+
+#for item in RcgJsonKeys:
+#    rcg_file.save_pot("translation", item)
+
+#for item in RcgJsonKeys:
+#    rcg_file.save_po("translation", item, "ru")
+
+# print(RcgJsonKeys.TUTORIAL_KEYS.value)
+# print(RcgLanguages.LANG_ENGLISH.value["iso_code"])
